99c249cf2439df9f3f5683e7d1fec6ec806b28ac
MIZUKI FUJITA (DOCU-IRIS-ISD-OOCLL/MNL) <FUJITMI@OOCL.COM>
2019-10-07 20:31:22 +0800

    Incomplete Task

--- main/main.js
+++ main/main.js
@@ -2,18 +2,30 @@ module.exports = function main(inputs) {
     var result = '';
     var current = '';
     var unit = '';
+    var quantity = 0;
+    var price = 0;
+    var subTotal = 0;
+    var total = 0;
     var topMessage = '***<store earning no money>Receipt ***\n';
     var botMessage1 = '----------------------\n';
     var botMessage2 = '**********************\n';
 
     inputs.forEach(element => {
-        var quantity = 0;
-        if (current = '') {
-            if (quantity == 0) {
-                current = element.Name;
-                quantity = 0;
-                result += "Name: " + element.Name + ", Quantity: " + quantity + ", Unit price: " + element.Price.toFixed(2) + " (yuan), Subtotal: ";
-            }
+        if (current == '') {
+            current = element.Name;
+            price = element.Price;
+            unit = element.Unit;   
+        }
+
+        if (element.Name == current) {
+            quantity++;
+            subTotal += price;
+        } else {
+            result += "Name: " + current + ", Quantity: " + quantity + ", Unit price: " + price.toFixed(2) + " (yuan), Subtotal: " + subTotal.toFixed(2) + " (yuan)";
+            current = '';
+            price = 0;
+            unit = '';
+            subTotal = 0;
         }
     });
     
