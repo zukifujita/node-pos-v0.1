e4f2fe91a093c6e3a83a9c51a53a91914731e019
MIZUKI FUJITA (DOCU-IRIS-ISD-OOCLL/MNL) <FUJITMI@OOCL.COM>
2019-10-07 13:31:22 +0800

    Exercise Day 1 (Node JS)

--- main/main.js
+++ main/main.js
@@ -1,4 +1,21 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(inputs) {
+    var result = '';
+    var current = '';
+    var quantity = 0;
+    var topMessage = '***<store earning no money>Receipt ***\n';
+
+    for (let value of Object.values(inputs)) {
+        if (value.Name != current) {
+            if (quantity > 0) {
+                current = value.Name;
+            } else {
+                quantity++;
+            }
+            result += "Name: " + value.Name + ", Quantity: " + quantity + ", Unit price: " + value.Price + " (yuan), Subtotal: ";
+            quantity = 1;
+        } 
+    }
+
+    console.log(result)
+    return result;
 };
