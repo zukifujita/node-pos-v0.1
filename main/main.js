24001bfd01a26e8707125b0814ebbb808ccc3308
MIZUKI FUJITA (DOCU-IRIS-ISD-OOCLL/MNL) <FUJITMI@OOCL.COM>
2019-10-07 17:47:33 +0800

    Incomplete activity

--- main/main.js
+++ main/main.js
@@ -1,21 +1,25 @@
 module.exports = function main(inputs) {
     var result = '';
     var current = '';
-    var quantity = 0;
     var topMessage = '***<store earning no money>Receipt ***\n';
+    var botMessage1 = '----------------------\n';
+    var botMessage2 = '**********************\n';
 
-    for (let value of Object.values(inputs)) {
-        if (value.Name != current) {
-            if (quantity > 0) {
-                current = value.Name;
-            } else {
+    inputs.forEach(element => {
+        var quantity = 0;
+        if (element.Name != current) {
+            if (quantity == 0) {
+                current = element.Name;
+                quantity = 0;
+            }
+            else {
                 quantity++;
             }
-            result += "Name: " + value.Name + ", Quantity: " + quantity + ", Unit price: " + value.Price + " (yuan), Subtotal: ";
-            quantity = 1;
-        } 
-    }
+            result += "Name: " + element.Name + ", Quantity: " + quantity + ", Unit price: " + element.Price.toFixed(2) + " (yuan), Subtotal: ";
+        }
 
+    });
+    
     console.log(result)
     return result;
 };
