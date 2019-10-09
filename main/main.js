b6ffd36f001318dcfbb2a0a654c3c6d942b2039f
Mizuki Fujita <mizuki.fujita@oocl.com>
2019-10-10 08:30:43 +0900

    Completed

--- main/main.js
+++ main/main.js
@@ -1,33 +1,63 @@
+'use strict';
 module.exports = function main(inputs) {
-    var result = '';
+    var result = '***<store earning no money>Receipt ***\n';
     var current = '';
     var unit = '';
     var quantity = 0;
     var price = 0;
     var subTotal = 0;
     var total = 0;
-    var topMessage = '***<store earning no money>Receipt ***\n';
     var botMessage1 = '----------------------\n';
     var botMessage2 = '**********************\n';
 
-    inputs.forEach(element => {
+    for (var i = 0; i < inputs.length; i++) {
         if (current == '') {
-            current = element.Name;
-            price = element.Price;
-            unit = element.Unit;   
+            current = inputs[i].Name;
+            price = inputs[i].Price;
+            unit = inputs[i].Unit;   
         }
 
-        if (element.Name == current) {
+        if (inputs[i].Name == current) {
             quantity++;
             subTotal += price;
+            if (i + 1 == inputs.length) {
+                total += subTotal;
+                if (unit == 'bottle') {
+                    result += "Name: " + current + ", Quantity: " + quantity + ' ' + unit +
+                    "s, Unit price: " + price.toFixed(2) + " (yuan), Subtotal: " + subTotal.toFixed(2) + " (yuan)\n";
+                } else if (unit.match(/^[A-Za-z]{1}$/g)) {
+                    result += "Name: " + current + ", Quantity: " + quantity + unit.replace(/^[A-Za-z]{1}$/g, "")+ 
+                    ", Unit price: " + price.toFixed(2) + " (yuan), Subtotal: " + subTotal.toFixed(2) + " (yuan)\n";
+                } else {
+                    result += "Name: " + current + ", Quantity: " + quantity + ' ' + unit + 
+                    ", Unit price: " + price.toFixed(2) + " (yuan), Subtotal: " + subTotal.toFixed(2) + " (yuan)\n";
+                }
+            }
         } else {
-            result += "Name: " + current + ", Quantity: " + quantity + ", Unit price: " + price.toFixed(2) + " (yuan), Subtotal: " + subTotal.toFixed(2) + " (yuan)";
+            total += subTotal;
+            if (unit == 'bottle') {
+                result += "Name: " + current + ", Quantity: " + quantity + ' ' + unit +
+                "s, Unit price: " + price.toFixed(2) + " (yuan), Subtotal: " + subTotal.toFixed(2) + " (yuan)\n";
+            } else if (unit.match(/^[A-Za-z]{1}$/g)) {
+                result += "Name: " + current + ", Quantity: " + quantity + unit.replace(/^[A-Za-z]{1}$/g, "") + 
+                ", Unit price: " + price.toFixed(2) + " (yuan), Subtotal: " + subTotal.toFixed(2) + " (yuan)\n";
+            } else {
+                result += "Name: " + current + ", Quantity: " + quantity + ' ' + unit + 
+                ", Unit price: " + price.toFixed(2) + " (yuan), Subtotal: " + subTotal.toFixed(2) + " (yuan)\n";
+            }
+            i -= 1;
             current = '';
             price = 0;
             unit = '';
             subTotal = 0;
+            quantity = 0;
         }
-    });
+        if(i + 1 == inputs.length){
+            result += botMessage1;
+            result += 'Total: ' + total.toFixed(2) + ' (yuan)\n';
+            result += botMessage2;
+        }
+    }
     
     console.log(result)
     return result;
